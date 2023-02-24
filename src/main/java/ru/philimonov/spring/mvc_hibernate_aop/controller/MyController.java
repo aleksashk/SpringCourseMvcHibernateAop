71cfd453eb3a3c3555a0e523a64627a2d23fa2e3
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-24 18:28:16 +0300

    edit an employee

--- src/main/java/ru/philimonov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/ru/philimonov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -5,11 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.philimonov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.philimonov.spring.mvc_hibernate_aop.entity.Employee;
 import ru.philimonov.spring.mvc_hibernate_aop.service.EmployeeService;
 
-import javax.jws.WebParam;
 import java.util.List;
 
 @Controller
@@ -37,5 +36,13 @@ public class MyController {
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.saveEmployee(employee);
         return "redirect:/";
+    }
+
+    @RequestMapping("/updateInfo")
+    public String updateEmployee(@RequestParam("empId") int id, Model model) {
+
+        Employee employee = employeeService.getEmployee(id);
+        model.addAttribute("employee", employee);
+        return "employee-info";
     }
 }
